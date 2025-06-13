39741c776ce2be48aec8767b7d9941a2dbf5c9f1
Yelizaveta Timchenko <yelizaveta.timchenko21@gmail.com>
2025-06-13 14:45:53 +0300

    Updated documentUtils.ts

--- src/utils/documentUtils.ts
+++ src/utils/documentUtils.ts
@@ -30,32 +30,6 @@ export const clearDocumentHeadersAndFooters = async (context: Word.RequestContex
 };
 
 export const insertDocument = async (context: Word.RequestContext, base64String: string) => {
-  // clear document headers and footers
-  const sections = context.document.sections;
-  context.load(sections, "items");
-
-  await context.sync();
-
-  for (const section of sections.items) {
-    const header = section.getHeader("Primary");
-    const footer = section.getFooter("Primary");
-
-    header.clear();
-    footer.clear();
-
-    const firstPageHeader = section.getHeader("FirstPage");
-    const firstPageFooter = section.getFooter("FirstPage");
-
-    firstPageHeader.clear();
-    firstPageFooter.clear();
-
-    const evenPageHeader = section.getHeader("EvenPages");
-    const evenPageFooter = section.getFooter("EvenPages");
-
-    evenPageHeader.clear();
-    evenPageFooter.clear();
-  }
-
   // clear document body
   context.document.body.clear();
 
